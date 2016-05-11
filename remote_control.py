7d8d4f519ba2aec09a4c60a4400bb6b57df1b091
fpetric <f5r1c.1m0@gmail.com>
2016-05-11 17:50:14 +0200

    Functional version, motion control is enabled using analog sticks of the joystick. Robot can be woken up and put to rest position using buttons.

--- remote_control.py
+++ remote_control.py
@@ -3,6 +3,7 @@ import math
 from pygame import locals
 from naoqi import ALProxy
 
+
 def dead_zone(x_, y_, epsilon):
     if abs(x_) < epsilon:
         x_ = 0.
@@ -25,9 +26,9 @@ started = False
 enabled = False
 
 try:
-    #behaviour = ALProxy("ALBehaviorManager", NAO_IP, NAO_PORT)
-    #motion = ALProxy("ALMotion", NAO_IP, NAO_PORT)
-    #memory = ALProxy("ALMemory", NAO_IP, NAO_PORT)
+    behaviour = ALProxy("ALBehaviorManager", NAO_IP, NAO_PORT)
+    motion = ALProxy("ALMotion", NAO_IP, NAO_PORT)
+    memory = ALProxy("ALMemory", NAO_IP, NAO_PORT)
     print("Connected to the robot")
 except:
     print("Error connecting to NAOqi on the robot")
@@ -40,47 +41,39 @@ try:
 
     while True:
         for e in pygame.event.get():
-            print(e)
-            if e.type == pygame.locals.JOYBUTTONUP:
-                if e.dict['button'] == 6:
-                    enabled = False
-            if e.type == pygame.locals.JOYBUTTONDOWN:
-                button = e.dict['button']
-                if button == 6 and started:
+            if not enabled:
+                if e.type == pygame.locals.JOYBUTTONDOWN and e.dict['button'] == 9:
+                    print('Remote control enabled')
                     enabled = True
-                if button == 0 and started:
-                    pass
-                if button == 1 and started:
-                    pass
-                if button == 2 and started:
-                    pass
-                if button == 3 and started:
-                    pass
-                if button == 9:
-                    if not started:
-                        print('Remote control enabled')
+            else:
+                if e.type == pygame.locals.JOYBUTTONUP and e.dict['button'] == 6:
+                    started = False
+                    print('Control of the motion of the robot disabled')
+
+                if e.type == pygame.locals.JOYBUTTONDOWN:
+                    button = e.dict['button']
+                    if button == 6:
                         started = True
-                    else:
+                        print('Control of the motion of the robot enabled')
+                    if button == 0:
+                        motion.wakeUp()
+                    if button == 1:
+                        pass
+                    if button == 2:
+                        pass
+                    if button == 3:
+                        motion.rest()
+                    if button == 9:
                         print('Remote control disabled')
-                        started = False
-                if button == 8:
-                    print('Shutting down remote control')
-                    exit()
-            if e.type == pygame.locals.JOYAXISMOTION and started:
-                axis = e.dict['axis']
-                if axis == 0 or axis == 1:
-                    x, y = dead_zone(joy.get_axis(1), -joy.get_axis(0), 0.05)
-                    theta = math.atan2(y, x)
-                    # theta = calculateSafeTurn(theta, turnAllowed)
-                    #motion.setWalkTargetVelocity(0, 0, theta / math.pi, 0.75)
-                elif axis == 2 or axis == 3:
-                    x, y = dead_zone(-joy.get_axis(3), joy.get_axis(2), 0.05)
-                    # x, y = calculateSafeXY(x, y, leftAllowed, rightAllowed)
-                    if x > 0:
-                        forward = True
-                    else:
-                        forward = False
-                    #motion.setWalkTargetVelocity(x, 0, 0, speed, gaitConfig)
+                        enabled = False
+                    if button == 8:
+                        print('Shutting down remote control')
+                        exit()
+                if e.type == pygame.locals.JOYAXISMOTION and motion.robotIsWakeUp() and enabled:
+                    x_turn, y_turn = dead_zone(joy.get_axis(1), -joy.get_axis(0), 0.05)
+                    theta = math.atan2(y_turn, x_turn)
+                    x, y = dead_zone(-joy.get_axis(3), -joy.get_axis(2), 0.05)
+                    motion.moveToward(x, y, theta/math.pi)
 
 except KeyboardInterrupt:
     exit()
