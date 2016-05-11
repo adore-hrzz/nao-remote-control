6e8f1f17a6a5fa8207afe853c4698a005075480b
fpetric <f5r1c.1m0@gmail.com>
2016-05-11 16:53:00 +0200

    Started working on the new version, updated README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nao-remote-control
+
+Remote control of NAO robot using Logitech F710 remote controller.
+
+## Dependencies
+pygame
+
+## Important
+Keep joystick in D-mode
--- remote_control.py
+++ remote_control.py
@@ -3,12 +3,6 @@ import math
 from pygame import locals
 from naoqi import ALProxy
 
-DEFAULT_STEP = []
-HIGH_STEP = [["StepHeight", 0.04], ["MaxStepX", 0.08]]
-NORMAL_SPEED = 0.75
-SLOW_SPEED = 0.01
-
-
 def dead_zone(x_, y_, epsilon):
     if abs(x_) < epsilon:
         x_ = 0.
@@ -24,20 +18,16 @@ except:
     print("Error initializing pygame")
     raise
 
-NAO_IP = "192.168.1.103"
+NAO_IP = "192.168.1.102"
 NAO_PORT = 9559
 
-STAND = "Stand up"
-SIT = "Sit down"
-
 started = False
-standing = False
-forward = False
+enabled = False
 
 try:
-    behaviour = ALProxy("ALBehaviorManager", NAO_IP, NAO_PORT)
-    motion = ALProxy("ALMotion", NAO_IP, NAO_PORT)
-    memory = ALProxy("ALMemory", NAO_IP, NAO_PORT)
+    #behaviour = ALProxy("ALBehaviorManager", NAO_IP, NAO_PORT)
+    #motion = ALProxy("ALMotion", NAO_IP, NAO_PORT)
+    #memory = ALProxy("ALMemory", NAO_IP, NAO_PORT)
     print("Connected to the robot")
 except:
     print("Error connecting to NAOqi on the robot")
@@ -48,32 +38,24 @@ try:
     joy.init()
     print("Enabled joystick %s" % joy.get_name())
 
-    gaitConfig = DEFAULT_STEP
-    speed = NORMAL_SPEED
-
     while True:
-
         for e in pygame.event.get():
             print(e)
+            if e.type == pygame.locals.JOYBUTTONUP:
+                if e.dict['button'] == 6:
+                    enabled = False
             if e.type == pygame.locals.JOYBUTTONDOWN:
                 button = e.dict['button']
+                if button == 6 and started:
+                    enabled = True
                 if button == 0 and started:
-                    behaviour.runBehavior(SIT)
-                    motion.setStiffnesses("Body", 0.0)
-                    standing = False
+                    pass
                 if button == 1 and started:
-                    motion.setStiffnesses("Body", 1.0)
-                    behaviour.runBehavior(STAND)
-                    motion.walkInit()
-                    standing = True
+                    pass
                 if button == 2 and started:
-                    gaitConfig = HIGH_STEP
-                    speed = SLOW_SPEED
-                    print('Switching to high step')
+                    pass
                 if button == 3 and started:
-                    gaitConfig = DEFAULT_STEP
-                    speed = NORMAL_SPEED
-                    print('Switching to default step')
+                    pass
                 if button == 9:
                     if not started:
                         print('Remote control enabled')
@@ -84,13 +66,13 @@ try:
                 if button == 8:
                     print('Shutting down remote control')
                     exit()
-            if e.type == pygame.locals.JOYAXISMOTION and started and standing:
+            if e.type == pygame.locals.JOYAXISMOTION and started:
                 axis = e.dict['axis']
                 if axis == 0 or axis == 1:
                     x, y = dead_zone(joy.get_axis(1), -joy.get_axis(0), 0.05)
                     theta = math.atan2(y, x)
                     # theta = calculateSafeTurn(theta, turnAllowed)
-                    motion.setWalkTargetVelocity(0, 0, theta / math.pi, 0.75)
+                    #motion.setWalkTargetVelocity(0, 0, theta / math.pi, 0.75)
                 elif axis == 2 or axis == 3:
                     x, y = dead_zone(-joy.get_axis(3), joy.get_axis(2), 0.05)
                     # x, y = calculateSafeXY(x, y, leftAllowed, rightAllowed)
@@ -98,7 +80,7 @@ try:
                         forward = True
                     else:
                         forward = False
-                    motion.setWalkTargetVelocity(x, 0, 0, speed, gaitConfig)
+                    #motion.setWalkTargetVelocity(x, 0, 0, speed, gaitConfig)
 
 except KeyboardInterrupt:
     exit()
